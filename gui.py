c153aeb3d0eba145533d987454448c8f71c7f432
john.dev62 <john.dev62@outlook.fr>
2026-07-22 08:20:00 +0200

    Passe en v1.0.14

--- gui.py
+++ gui.py
@@ -18,7 +18,7 @@ from csv_export import export_time_entries_csv, export_invoices_csv
 
 APP_TITLE = "TempoFacture"
 DONATE_URL = "https://ko-fi.com/yoshines62000"
-APP_VERSION = "1.0.13"
+APP_VERSION = "1.0.14"
 UPDATE_REPO = "yoshines62000-alt/TempoFacture"
 RELEASES_URL = f"https://github.com/{UPDATE_REPO}/releases/latest"
 IDLE_THRESHOLD_SECONDS = 5 * 60  # valeur par defaut si aucun reglage n'existe encore en base
